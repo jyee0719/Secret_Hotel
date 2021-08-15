41eb15d361fbbe94964a99b5cb46a469305ae355
michelle <jyee0719@gmail.com>
2021-08-15 20:06:55 +0800

    fghj

---
+++ app/src/main/java/my/edu/utar/secret_hotel/RoomAdapter.java
@@ -0,0 +1,75 @@
+package my.edu.utar.secret_hotel;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+public class RoomAdapter extends BaseAdapter {
+
+    private final ArrayList<Room> roomArrayList;
+    private final Context context;
+    //update
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+    public RoomAdapter(ArrayList<Room> roomArrayList, Context context){
+        this.roomArrayList = roomArrayList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return this.roomArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        convertView = layoutInflater.inflate(R.layout.single_room, null);
+
+
+        TextView type = convertView.findViewById(R.id.roomname);
+        TextView desc = convertView.findViewById(R.id.roomdesc);
+        TextView price = convertView.findViewById(R.id.roomprice);
+        ImageView image = convertView.findViewById(R.id.roomimageview);
+
+
+        Room room = roomArrayList.get(position);
+        type.setText(room.getRoomType());
+        desc.setText(room.getRoomDesc());
+        price.setText(room.getRoomPrice());
+
+        Glide.with(context)
+                .load(room.getRoomImageURL())
+                .into(image);
+
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/my/edu/utar/secret_hotel/RoomList.java
+++ app/src/main/java/my/edu/utar/secret_hotel/RoomList.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,11 +21,12 @@ import java.util.ArrayList;
 
 public class RoomList extends AppCompatActivity {
 
-    RecyclerView recyclerView;
     private DatabaseReference databaseReference, root;
     ArrayList<Room> roomArrayList;
     CustomAdapter customAdapter;
     Context context;
+    RoomAdapter roomAdapter;
+    ListView listView;
 
 
 
@@ -33,66 +35,27 @@ public class RoomList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_list);
 
-        recyclerView=(RecyclerView) this.findViewById(R.id.recyclerView);
+        listView=findViewById(R.id.listview);
         root = FirebaseDatabase.getInstance().getReference("Rooms");
-//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1);
-//        recyclerView.setLayoutManager(gridLayoutManager);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);
 
         roomArrayList=new ArrayList<>();
-        customAdapter=new CustomAdapter(this,roomArrayList);
 
-//        Query query = databaseReference.child("Rooms");
-//        query.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                clearAll();
-//                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-////                    Room room = new Room();
-////                    room.setRoomType(dataSnapshot.child("type").getValue().toString());
-////                    room.setRoomDesc(dataSnapshot.child("description").getValue().toString());
-////                    room.setRoomImageURL(dataSnapshot.child("image").getValue().toString());
-////                    room.setRoomPrice(Double.parseDouble(dataSnapshot.child("price").getValue().toString()));
-////                    //Room room = dataSnapshot.getValue(Room.class);
-////                    roomArrayList.add(room);
-//                    String type = dataSnapshot.child("type").getValue().toString();
-//                    String description = dataSnapshot.child("description").getValue().toString();
-//                    String image = dataSnapshot.child("image").getValue().toString();
-//                    double price = Double.parseDouble(dataSnapshot.child("price").getValue().toString());
-//                    roomArrayList.add(new Room(type,description,image,price));
-//                }
-//                customAdapter = new CustomAdapter(getApplicationContext(), roomArrayList);
-//                customAdapter.notifyDataSetChanged();
-//                recyclerView.setAdapter(customAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
-
-
-            //ArrayList<Room> roomArrayList = new ArrayList<>();
             databaseReference = FirebaseDatabase.getInstance().getReference("Rooms");
             databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    clearAll();
                     for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                        //Room room = new Room();
-//                        room.setRoomType(dataSnapshot.child("type").getValue().toString());
-//                        room.setRoomDesc(dataSnapshot.child("description").getValue().toString());
-//                        room.setRoomImageURL(dataSnapshot.child("image").getValue().toString());
-//                        room.setRoomPrice(dataSnapshot.child("price").getValue().toString());
-                        Room room = dataSnapshot.getValue(Room.class);
+                        Room room = new Room();
+                        room.setRoomType(dataSnapshot.child("type").getValue().toString());
+                        room.setRoomDesc(dataSnapshot.child("description").getValue().toString());
+                        room.setRoomImageURL(dataSnapshot.child("image").getValue().toString());
+                        room.setRoomPrice(dataSnapshot.child("price").getValue().toString());
+                        //Room room = dataSnapshot.getValue(Room.class);
                         roomArrayList.add(room);
                     }
-                    //customAdapter=new CustomAdapter(RoomList.this,roomArrayList);
-                    recyclerView.setAdapter(customAdapter);
-                    customAdapter.notifyDataSetChanged();
+                    roomAdapter = new RoomAdapter(roomArrayList,RoomList.this);
+                    listView.setAdapter(roomAdapter);
+                    roomAdapter.notifyDataSetChanged();
 
                 }
 
@@ -101,17 +64,6 @@ public class RoomList extends AppCompatActivity {
 
                 }
             });
-        }
-
-        private void clearAll(){
-        if(roomArrayList!=null){
-            roomArrayList.clear();
-
-            if(customAdapter!=null){
-                customAdapter.notifyDataSetChanged();
-            }
-        }
-        roomArrayList = new ArrayList<>();
         }
 
 }
