e4ba8886f3b4d2b87dbe2577e185971421e3d3a7
mjjj5 <leemanjing@1utar.my>
2021-08-18 02:19:59 +0800

    check user login status

--- app/src/main/java/my/edu/utar/secret_hotel/MainActivity.java
+++ app/src/main/java/my/edu/utar/secret_hotel/MainActivity.java
@@ -7,15 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnMenu, btnProfile, btnBook, btnService;
+    FirebaseAuth fAuth;
+    DatabaseReference databaseReference;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = fAuth.getUid();
         btnMenu = findViewById(R.id.btnMenu);
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +39,17 @@ public class MainActivity extends AppCompatActivity {
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
+
+
+                if(userID != null){
+                    //User is signed in
+                    Intent intent = new Intent(getApplicationContext(),Profile.class);
+                    startActivity(intent);
+                } else{
+                    //No user is signed in
+                    Intent intent = new Intent(getApplicationContext(),Login.class);
+                    startActivity(intent);
+                }
             }
         });
 
